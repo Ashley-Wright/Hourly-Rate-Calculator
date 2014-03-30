52147a6ff5e58cc79658421b35640b852545c4d5
Ashley Wright <AshleyMarieEvans@gmail.com>
2014-03-30 11:50:41 -0500

    Add Rules into a table

--- main.js
+++ main.js
@@ -1,4 +1,21 @@
 $(document).ready(initialize);
 
 function initialize(){
+  $('#addRuleButton').click(addRule);
+}
+
+function addRule(){
+  // Get Values
+  var ruleTitle = $('#ruleTitle').val();
+  var ruleValue = parseInt($('#ruleValue').val());
+
+  // Add Row to Rules Table
+  var $tr = $('<tr>');
+  var $checkbox = $('<input type="checkbox">');
+  var $title = $('<td class="tdTitle">');
+  $title.text(ruleTitle);
+  var $value = $('<td class="tdValue">');
+  $value.text(ruleValue);
+  $tr.append($checkbox, $title, $value);
+  $('table').append($tr);
 }
